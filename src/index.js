53227040513924922de3e23e9ce8549749c482f7
alvinjtang <alvintangdesigns@gmail.com>
2019-08-07 23:01:46 -0400

    extending schema definitions

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs = `
+type Query {
+    info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
+}
+`;
+
+const resolvers = {
+    Query: {
+        info: () => null
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+});
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
