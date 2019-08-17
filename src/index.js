a157392f517e84fd37caa5079cc9d2255c46c579
alvinjtang <alvintangdesigns@gmail.com>
2019-08-17 13:42:18 -0400

    intro to mutations

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ type Query {
     feed: [Link!]!
 }
 
+type Mutation {
+    post(url: String!, description: String!): Link!
+}
+
 type Link {
     id: ID!
     description: String!
@@ -36,4 +40,4 @@ const server = new GraphQLServer({
     resolvers,
 });
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.start(() => console.log('Server is running on http://localhost:4000'));
