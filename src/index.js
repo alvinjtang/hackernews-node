c0943da86b78641341fe2dc30bd5afde2656d7a5
alvinjtang <alvintangdesigns@gmail.com>
2019-08-21 11:33:11 -0400

    added single query, update mutation and delete mutation and setting up prisma config

--- src/index.js
+++ src/index.js
@@ -6,9 +6,14 @@ let links = [{
     description: 'Fullstack tutorial for GraphQL'
 }];
 
+let idCount = links.length;
+
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
+        link: (parent, args) => {
+            return links.filter(x => x.id === args.id)[0]
+        },
         feed: () => links,
     },
     Mutation: {
@@ -20,13 +25,33 @@ const resolvers = {
             }
             links.push(link)
             return link
+        },
+        updateLink: (parent, args) => {
+            const updatedLink = {
+                id: args.id,
+                description: args.description,
+                url: args.url,
+            }
+            for (let i = 0; i < links.length; i++) {
+                if (links[i].id === args.id) {
+                    links[i] = {...links[i], ...updatedLink}
+                }
+            }
+            return updatedLink;
+        },
+        deleteLink: (parent, args) => {
+            let has = false;
+            for (let i = 0; i < links.length; i++) {
+                if (links[i].id === args.id) has = true;
+            }
+            if (has) {
+                for (let i = 0; i < links.length; i++) {
+                    links = links.filter(x => x.id !== args.id)
+                }
+            }
+            return;
         }
     },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-    }
 };
 
 const server = new GraphQLServer({
